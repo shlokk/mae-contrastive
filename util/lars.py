552b7ad1255c5711609ea09b0c8581170cb83201
Shlok Mishra <shlokm@vulcan08.umiacs.umd.edu>
2023-04-03 16:37:11 -0400

    clean up code

--- util/lars.py
+++ util/lars.py
@@ -1,13 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# --------------------------------------------------------
-# LARS optimizer, implementation from MoCo v3:
-# https://github.com/facebookresearch/moco-v3
-# --------------------------------------------------------
-
 import torch
 
 
